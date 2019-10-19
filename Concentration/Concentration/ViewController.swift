e3b75109a03b6cd5f8cde56f4abea01b503e1d2c
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2019-10-19 12:48:32 +0300

    initial commit

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Berk Batuhan ŞAKAR on 19.10.2019.
+//  Copyright © 2019 Berk Batuhan ŞAKAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
