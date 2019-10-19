ad96d4e7e89f43c30b4551fb6066b661e78f5846
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2019-10-20 01:36:16 +0300

    random cards added.

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -9,28 +9,64 @@
 import Foundation
 
 class Concentration {
+    
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        //v1
-        //        if cards[index].isFaceUP {
-        //            cards[index].isFaceUP = false
-        //        } else {
-        //            cards[index].isFaceUP = true
-        //        }
-        
-        //v2
-        cards[index].isFaceUP = !cards[index].isFaceUP
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+          cards[index].isFaceUP = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }else {
+                //either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUP = false
+                }
+                cards[index].isFaceUP = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        } else {
+            cards[index].isFaceUP = false
+        }
+        //cards[index].isFaceUP = !cards[index].isFaceUP
         
     }
     
     init(numberOfPairsOfCards: Int) {
+        var unShuffleCards: [Card] = []
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
-            cards += [card, card]
+//            cards += [card, card]
+            unShuffleCards += [card, card]
         }
         
         //TODO: Shuffle the cards - Kartları karıştır.
-        
+//        cards.shuffle()
+        while !unShuffleCards.isEmpty {
+            let randomIndex = unShuffleCards.count.arc4Random
+            let card = unShuffleCards.remove(at: randomIndex)
+            cards.append(card)
+        }
+    }
+}
+
+
+extension Int {
+    var arc4Random: Int {
+        switch self {
+        case 1...Int.max:
+            return Int(arc4random_uniform(UInt32(self)))
+        case -Int.max..<0:
+            return Int(arc4random_uniform(UInt32(self)))
+        default:
+            return 0
+        }
     }
 }
