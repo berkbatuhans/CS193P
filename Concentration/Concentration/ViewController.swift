d953556e5b8c48202e717919dddc0b6b49088177
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2019-10-19 14:08:14 +0300

    flipcard method and didset added.
    flipcard method and didset added.

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! a ghost!")
+    
+    var flipCount = 0 {
+        didSet {
+         flipCountLabel.text = "Çevirmeler: \(flipCount)"
+        }
     }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func flipCard(withEmoji emoji: String,on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 
 
